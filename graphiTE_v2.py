decb33a4f661bb9e4c03ca4cde1e42febe89763a
Queenie Li <>
2023-04-14 10:52:55 -0700

    Adding new graphiTE version with neighbors implemented

---
+++ graphiTE_v2.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+
+# load csv file in df
+data = pd.read_csv('/Users/queenie1/Desktop/kimlab/data/graphiTE_shuf100_test.csv')
+
+# get rid of duplicates
+genes = data['name2'].unique()
+repeats = data['repName'].unique()
+# classifications = data['classification'].unique()
+
+# create 3D numpy array initialized with zeros
+array_3d = np.full((len(genes), len(repeats)), 0)
+
+# loop through the rows of the data and fill the array with 1s where there's a match
+for index, row in data.iterrows():
+    gene_index = np.where(genes == row['name2'])[0][0]
+    repeat_index = np.where(repeats == row['repName'])[0][0]
+    # classification_index = np.where(classifications == row['classification'])[0][0]
+    array_3d[gene_index, repeat_index] = 1
+
+def compare(gene):
+
+    # get position of gene
+    gene_pos = np.where(genes == gene)[0][0]
+
+    # initialize empty set to store neighbors
+    neighbors = set()
+
+    # loop by columns, rows
+    for TE in range(len(repeats)):
+        for gene_row in range(len(genes)):
+            # make sure we only do meaningful comparisons
+            if gene_pos == gene_row or array_3d[gene_pos][TE] == 0:
+                continue
+            # add index to neighbors set
+            if array_3d[gene_row][TE] == 1:
+                neighbors.add(gene_row)
+
+    return neighbors
+
+class Graph:
+    gene_obj = genes
+
+class Gene:
+
+    def __init__(self, gene_name):
+        self.gene_name = gene_name
+
+        self.neighbors = compare(self.gene_name)
+
+    def __repr__(self):
+        return f"{self.gene_name}"
+
+def main():
+    graph = Graph()
+
+    for gene_index in range(len(graph.gene_obj)):
+        node = Gene(graph.gene_obj[gene_index])
+        if len(node.neighbors) > 0:
+            print('{} {} {}'.format(gene_index, node.gene_name, node.neighbors))
+
+main()
+
+
+
+
